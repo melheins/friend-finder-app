9e35841055867cee0d1ff2cdedd9f2a96c160972
melheins <heins.mel@gmail.com>
2017-11-11 09:48:39 -0500

    update

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+// Array with all friends data
+var friendsArray = [
+    {
+        name: 'Tom',
+        photo: 'http://via.placeholder.com/250x250',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Jerry',
+        photo: 'http://via.placeholder.com/250x250',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    }
+];
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+// Pull in required dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-// Sets up the Express App
-// =============================================================
+// Configure the Express application
 var app = express();
-var PORT = 3000;
+var PORT = 30000;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Add middleware for parsing incoming request bodies
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function () {
+    console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
